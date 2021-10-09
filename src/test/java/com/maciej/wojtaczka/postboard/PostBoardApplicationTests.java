871e2443dcee245e6c6edb7ce6e13cac71235b77
mwojtacz <mwojtacz@akamai.com>
2021-10-09 17:48:26 +0200

    Init

---
+++ src/test/java/com/maciej/wojtaczka/postboard/PostBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maciej.wojtaczka.postboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
