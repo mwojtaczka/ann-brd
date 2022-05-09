b197741c94a38cc856a997e1bed4cd5d9844d8bc
mwojtacz <mwojtacz@akamai.com>
2022-05-09 14:17:01 +0200

    Rename main class

--- src/main/java/com/maciej/wojtaczka/announcementboard/AnnouncementBoardApplication.java
+++ src/main/java/com/maciej/wojtaczka/announcementboard/AnnouncementBoardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PostBoardApplication {
+public class AnnouncementBoardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PostBoardApplication.class, args);
+        SpringApplication.run(AnnouncementBoardApplication.class, args);
     }
 
 }
